9a586e3da1c7c97597ff27cf1d099e023ac1d34e
murakou1 <e1b21105@oit.ac.jp>
2023-09-26 15:46:23 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1925/kaizi/janken/JankenApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z1925.kaizi.janken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankenApplication.class, args);
+	}
+
+}
