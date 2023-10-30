dbff3d2d891e49bca6f2619a5e906fa19d1efa88
murakou1 <e1b21105@oit.ac.jp>
2023-10-30 16:39:19 +0900

    内容のリセット

---
+++ src/main/java/oit/is/z1925/kaizi/janken/model/UserMapper.java
@@ -0,0 +1,28 @@
+package oit.is.z1925.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+  @Select("SELECT id from users")
+  ArrayList<User> selectAllUsers();
+
+  @Select("SELECT ID, NAME FROM USERS WHERE ID = #{id}")
+  User selectById(int id);
+
+  /**
+   * #{userName}などはinsertの引数にあるChamberクラスのフィールドを表しています 引数に直接String
+   * userNameなどと書いてもいけるはず
+   * 下記のOptionsを指定すると，insert実行時にAuto incrementされたIDの情報を取得できるようになる useGeneratedKeys
+   * = true -> Keyは自動生成されることを表す keyColumn : keyになるテーブルのカラム名 keyProperty :
+   * keyになるJavaクラスのフィールド名
+   *
+   */
+
+}
