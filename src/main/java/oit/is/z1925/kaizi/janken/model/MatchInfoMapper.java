3c067370e57f56810b229f3c5b35d0a8636a3c24
murakou1 <e1b21105@oit.ac.jp>
2023-11-07 09:49:14 +0900

    相手を選び手を選んだ際にデータベースにINSERTする

---
+++ src/main/java/oit/is/z1925/kaizi/janken/model/MatchInfoMapper.java
@@ -0,0 +1,17 @@
+package oit.is.z1925.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface MatchInfoMapper {
+
+  @Insert("INSERT INTO matchInfo (user1,user2,user1Hand,isActive) VALUES (#{user1},#{user2},#{user1Hand},#{isActive})")
+  @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
+  void insertMatchInfo(MatchInfo matchInfo);
+
+}
